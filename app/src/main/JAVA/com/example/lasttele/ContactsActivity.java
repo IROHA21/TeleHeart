c45abd0f2092ab4d0daeadebeff688a9ef9bd57a
IROHA21 <mouniraitaissa1@gmail.com>
2025-03-05 13:31:32 +0300

    added a return button works bad also need to adjust button size for it to be always the same

--- app/src/main/JAVA/com/example/lasttele/ContactsActivity.java
+++ app/src/main/JAVA/com/example/lasttele/ContactsActivity.java
@@ -1,6 +1,8 @@
 package com.example.lasttele;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -129,5 +131,13 @@ public class ContactsActivity extends AppCompatActivity {
                 Toast.makeText(this, "No contact selected", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+    public void onbuttonclick(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+
+
+
+
     }
 }
